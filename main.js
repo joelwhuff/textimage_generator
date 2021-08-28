3fde62ba16db6fbb9a091c0f5c83a22f69a7f40e
Joel Huff <joelhuff98@gmail.com>
2021-08-28 02:27:37 -0400

    first and last commit?

---
+++ main.js
@@ -0,0 +1,109 @@
+// todo: make japanese character string
+
+const body = document.body;
+const root = document.getElementById("root");
+
+window.addEventListener("dragenter", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    body.style.backgroundColor = "#ddd";
+});
+window.addEventListener("dragover", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+});
+window.addEventListener("dragleave", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    if (e.x === 0 && e.y === 0) body.style.backgroundColor = "#fff";
+});
+window.addEventListener("drop", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    body.style.backgroundColor = "#fff";
+
+    onFileDrop(e.dataTransfer.files[0]);
+});
+
+let image = document.createElement("img");
+let currentSize = 1;
+
+image.addEventListener("load", generateTextImage);
+
+document.addEventListener("keyup", (e) => {
+    const key = e.key;
+    if (key === "-") {
+        currentSize /= 1.2;
+        generateTextImage();
+    } else if (key === "+" || key === "=") {
+        currentSize *= 1.2;
+        generateTextImage();
+    }
+});
+
+function onFileDrop(file) {
+    const reader = new FileReader();
+
+    if (!file.type.includes("image/")) return;
+
+    reader.onload = function (e) {
+        currentSize = 1;
+        image.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+}
+
+function generateTextImage() {
+    const imgData = getImgData();
+    convertImgDataToText(imgData);
+}
+
+function getImgData() {
+    const width = Math.round(image.width * currentSize);
+    const height = Math.round(image.height * currentSize);
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = width;
+    canvas.height = height;
+
+    ctx.drawImage(image, 0, 0, width, height);
+
+    return ctx.getImageData(0, 0, width, height);
+}
+
+function convertImgDataToText(imgData) {
+    const textContainer = document.createElement("div");
+    textContainer.classList.add("text-cntr");
+
+    const symbols = 'WBOwxc+*",-.  ';
+    const symbolsLastIndex = symbols.length - 1;
+
+    const dataWidth = imgData.width * 4;
+    const imgHeight = imgData.height;
+    const pixelData = imgData.data;
+
+    for (let i = 0; i < imgHeight; ++i) {
+        const textLine = document.createElement("div");
+        let text = "";
+
+        for (let j = i * dataWidth; j < dataWidth * (i + 1); j += 4) {
+            let total = pixelData[j] + pixelData[j + 1] + pixelData[j + 2];
+            text += symbols[Math.floor((total / 765) * symbolsLastIndex)];
+        }
+        textLine.textContent = text;
+
+        textContainer.appendChild(textLine);
+    }
+
+    while (root.firstChild) {
+        root.removeChild(root.firstChild);
+    }
+
+    const tip = document.createElement("div");
+    tip.classList.add("tip");
+    tip.textContent = "Press +/- to resize the image";
+
+    root.appendChild(tip);
+    root.appendChild(textContainer);
+}
